5cd3b2da74c7983c71ab808ffc2c9eb76b2fcb70
Jason J Cruz <jaycru06@yahoo.com>
2019-12-09 03:49:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -19,12 +19,12 @@ class Anagram
       word = word.split("")
       if word.sort == anagram_word.split("").sort 
         match_word << word
-        match_word.join("").split(",")
+        match_word.split(",")
       else 
         match_word 
       end 
     end 
-    match_word.flatten
+    match_word.join.split.flatten
   end 
   
 end 
