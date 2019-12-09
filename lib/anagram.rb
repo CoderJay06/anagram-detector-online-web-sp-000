ef0d973d6ef47f37bb41e4351e1a035f8f1ca432
Jason J Cruz <jaycru06@yahoo.com>
2019-12-09 02:49:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :anagram_word 
+  
+  def initialize(anagram_word)
+    @anagram_word = anagram_word
+  end 
+  
+  def match 
+  end 
+  
+end 
