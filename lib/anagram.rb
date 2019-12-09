476668e029c456d6545e84baaacbf5ce6bb1b185
Jason J Cruz <jaycru06@yahoo.com>
2019-12-09 03:01:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,12 @@ class Anagram
   end 
   
   def match(words)
-    words = %w()
+    # iterate over array of words 
+    # compare each word of words to anagram_word 
+    # determine if they are composed of same letters
+    # split can make word into array of letters 
+    # compare 2 arrays using == 
+    words
   end 
   
 end 
